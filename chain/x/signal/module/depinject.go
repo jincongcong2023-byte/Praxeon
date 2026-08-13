1eee63794645ff466910226b6ae8ba1e6407f704
Riemann_Jin <jincongcong2023@gmail.com>
2026-08-13 23:17:32 +0800

    Add chain/x/signal/module/depinject.go

---
+++ chain/x/signal/module/depinject.go
@@ -0,0 +1,65 @@
+package signal
+
+import (
+	"cosmossdk.io/core/address"
+	"cosmossdk.io/core/appmodule"
+	"cosmossdk.io/core/store"
+	"cosmossdk.io/depinject"
+	"cosmossdk.io/depinject/appconfig"
+	"github.com/cosmos/cosmos-sdk/codec"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+
+	"mars/x/signal/keeper"
+	"mars/x/signal/types"
+)
+
+var _ depinject.OnePerModuleType = AppModule{}
+
+// IsOnePerModuleType implements the depinject.OnePerModuleType interface.
+func (AppModule) IsOnePerModuleType() {}
+
+func init() {
+	appconfig.Register(
+		&types.Module{},
+		appconfig.Provide(ProvideModule),
+	)
+}
+
+type ModuleInputs struct {
+	depinject.In
+
+	Config       *types.Module
+	StoreService store.KVStoreService
+	Cdc          codec.Codec
+	AddressCodec address.Codec
+
+	AuthKeeper   types.AuthKeeper
+	BankKeeper   types.BankKeeper
+	IdentityKeeper types.IdentityKeeper
+}
+
+type ModuleOutputs struct {
+	depinject.Out
+
+	SignalKeeper keeper.Keeper
+	Module       appmodule.AppModule
+}
+
+func ProvideModule(in ModuleInputs) ModuleOutputs {
+	// default to governance authority if not provided
+	authority := authtypes.NewModuleAddress(types.GovModuleName)
+	if in.Config.Authority != "" {
+		authority = authtypes.NewModuleAddressOrBech32Address(in.Config.Authority)
+	}
+	k := keeper.NewKeeper(
+		in.StoreService,
+		in.Cdc,
+		in.AddressCodec,
+		authority,
+		in.BankKeeper,
+		in.IdentityKeeper,
+	)
+	m := NewAppModule(in.Cdc, k, in.AuthKeeper, in.BankKeeper)
+
+	return ModuleOutputs{SignalKeeper: k, Module: m}
+}
