832b898ce22fee519f009171df7569dd2caf67ee
Riemann_Jin <jincongcong2023@gmail.com>
2026-08-13 23:14:30 +0800

    Add chain/x/prax/types/expected_keepers.go

---
+++ chain/x/prax/types/expected_keepers.go
@@ -0,0 +1,34 @@
+package types
+
+import (
+	"context"
+
+	"cosmossdk.io/core/address"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// AuthKeeper defines the expected interface for the Auth module.
+type AuthKeeper interface {
+	AddressCodec() address.Codec
+	GetAccount(context.Context, sdk.AccAddress) sdk.AccountI // only used for simulation
+	// Methods imported from account should be defined here
+}
+
+// BankKeeper defines the expected interface for the Bank module.
+type BankKeeper interface {
+	SpendableCoins(context.Context, sdk.AccAddress) sdk.Coins
+	MintCoins(context.Context, string, sdk.Coins) error
+	GetSupply(context.Context, string) sdk.Coin
+	SendCoinsFromModuleToAccount(context.Context, string, sdk.AccAddress, sdk.Coins) error
+}
+
+// SignalKeeper defines the expected interface for the Signal module.
+type SignalKeeper interface {
+	IterateSignalWeights(context.Context, func(addr string, weight uint64) error) error
+}
+
+// ParamSubspace defines the expected Subspace interface for parameters.
+type ParamSubspace interface {
+	Get(context.Context, []byte, interface{})
+	Set(context.Context, []byte, interface{})
+}
